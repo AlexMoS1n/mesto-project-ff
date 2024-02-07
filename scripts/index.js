b0ae686779ad09badb0797d48c3b09efb0e5d308
AlexMos1n <alex-mos777@yandex.ru>
2024-02-07 15:31:53 +0300

    Version 2.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,25 @@
-// @todo: Темплейт карточки
-
-// @todo: DOM узлы
-
-// @todo: Функция создания карточки
-
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
 const placesList = document.querySelector('.places__list');
+const cardTemplate = document.querySelector('#card-template').content;
 
 initialCards.forEach(function(item){
-  addPlace(item.link, item.name)
+  addCard(createCard(item.link, item.name, deleteCard));
 });
 
-function addPlace(srcAddress,altDescription){
-  const cardTemplate = document.querySelector('#card-template').content;
-  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true); 
-  const cardImage = cardElement.querySelector('.card__image'); //Здесь лучше использовать переменную, т.к. лишний запрос через querySelector отнимает ресурсы 
-  cardImage.src = srcAddress;
-  cardImage.alt = altDescription;
-  cardElement.querySelector('.card__title').textContent=altDescription;
-  cardElement.querySelector('.card__delete-button').addEventListener('click', delCard);
-  placesList.append(cardElement);
+function addCard(card) {
+  placesList.append(card);
 }
 
-function delCard (evt){
-  const listItem = evt.target.closest('.places__item');
-  listItem.remove();
+function createCard(srcAddress, altTitle, deleteFunction){
+  const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true); 
+  const cardImage = cardElement.querySelector('.card__image'); 
+  cardImage.src = srcAddress;
+  cardImage.alt = altTitle;
+  cardElement.querySelector('.card__title').textContent=altTitle;
+  cardElement.querySelector('.card__delete-button').addEventListener('click', deleteFunction);
+  return cardElement;
+}
+
+function deleteCard (evt){
+  const card = evt.target.closest('.places__item');
+  card.remove();
 }
